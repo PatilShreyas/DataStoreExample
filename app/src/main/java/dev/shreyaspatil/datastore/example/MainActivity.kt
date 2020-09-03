0f88a83604f27c4187da775a0a2f43d888155283
Shreyas <shreyaspatilg@gmail.com>
2020-09-03 19:50:33 +0530

    Initial commit

---
+++ app/src/main/java/dev/shreyaspatil/datastore/example/MainActivity.kt
@@ -0,0 +1,64 @@
+package dev.shreyaspatil.datastore.example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.launch
+
+@Suppress("WHEN_ENUM_CAN_BE_NULL_IN_JAVA")
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var settingsManager: SettingsManager
+    private var isDarkMode = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        settingsManager = SettingsManager(applicationContext)
+
+        observeUiPreferences()
+        initViews()
+    }
+
+    private fun observeUiPreferences() {
+        settingsManager.uiModeFlow.asLiveData().observe(this) { uiMode ->
+            when (uiMode) {
+                UiMode.LIGHT -> onLightMode()
+                UiMode.DARK -> onDarkMode()
+            }
+        }
+    }
+
+    private fun initViews() {
+        imageButton.setOnClickListener {
+            lifecycleScope.launch {
+                when (isDarkMode) {
+                    true -> settingsManager.setUiMode(UiMode.LIGHT)
+                    false -> settingsManager.setUiMode(UiMode.DARK)
+                }
+            }
+        }
+    }
+
+    private fun onLightMode() {
+        isDarkMode = false
+
+        rootView.setBackgroundColor(ContextCompat.getColor(this, android.R.color.white))
+        imageButton.setImageResource(R.drawable.ic_moon)
+
+        // Actually turn on Light mode using AppCompatDelegate.setDefaultNightMode() here
+    }
+
+    private fun onDarkMode() {
+        isDarkMode = true
+
+        rootView.setBackgroundColor(ContextCompat.getColor(this, android.R.color.black))
+        imageButton.setImageResource(R.drawable.ic_sun)
+
+        // Actually turn on Dark mode using AppCompatDelegate.setDefaultNightMode() here
+    }
+}
---
+++ app/src/main/java/dev/shreyaspatil/datastore/example/SettingsManager.kt
@@ -0,0 +1,51 @@
+package dev.shreyaspatil.datastore.example
+
+import android.content.Context
+import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.edit
+import androidx.datastore.preferences.emptyPreferences
+import androidx.datastore.preferences.preferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+enum class UiMode {
+    LIGHT, DARK
+}
+
+class SettingsManager(context: Context) {
+
+    private val dataStore = context.createDataStore(name = "settings_pref")
+
+    val uiModeFlow: Flow<UiMode> = dataStore.data
+        .catch {
+            if (it is IOException) {
+                it.printStackTrace()
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }
+        .map { preference ->
+            val isDarkMode = preference[IS_DARK_MODE] ?: false
+
+            when (isDarkMode) {
+                true -> UiMode.DARK
+                false -> UiMode.LIGHT
+            }
+        }
+
+    suspend fun setUiMode(uiMode: UiMode) {
+        dataStore.edit { preferences ->
+            preferences[IS_DARK_MODE] = when (uiMode) {
+                UiMode.LIGHT -> false
+                UiMode.DARK -> true
+            }
+        }
+    }
+
+    companion object {
+        val IS_DARK_MODE = preferencesKey<Boolean>("dark_mode")
+    }
+}
